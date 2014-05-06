a2939954c260dc6a225a7afd20e686967c5ab2d4
vdyckn <vandyck.nick@outlook.com>
2014-05-06 08:16:45 +0200

    Initial commit

---
+++ PSNodeVersion.psm1
@@ -0,0 +1,48 @@
+﻿ function Get-EnvironmentVar
+ {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String]$Variable,
+        [ValidateSet("User", "Machine", "Process")]
+        [String]$Target
+    )
+    
+    $Target = @{$true="Process";$false=$Target}[$Target -eq $null -or $Target -eq ""]
+    Write-Output ([System.Environment]::GetEnvironmentVariable($Variable, $Target))
+ }
+
+
+
+function nvm 
+{
+    [CmdletBinding()]
+    Param
+    (
+    )
+
+    Begin
+    {
+    
+    }
+
+    Process
+    {
+    
+    }
+
+    End
+    {
+
+    }
+}
+
+#Prereq checks
+ if((Get-EnvironmentVar CurNodeVer Machine) -eq $null)
+ {
+    #[System.Environment]::SetEnvironmentVariable("CurNodeVer", "Stable", "Machine")
+    Write-Output "var not ccreated"
+ }
+
+Write-Output "PSNodeVersion module imported"
