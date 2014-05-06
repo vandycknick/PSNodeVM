6738b81a20ea7dd2d287d560fafcd1979cc7e524
vdyckn <vandyck.nick@outlook.com>
2014-05-06 18:21:09 +0200

    Updated all files and added info about current research

--- PSNodeVersion.psm1
+++ PSNodeVersion.psm1
@@ -1,5 +1,17 @@
-﻿ function Get-EnvironmentVar
- {
+﻿#Set default constant vars
+#Set nvmHome to  C:\Program Files\nodejs
+[String] $nvmHome = "C:\PSNodeJS\"
+[String] $nodeDist = "http://nodejs.org/dist/"
+[String] $npmDist = "$nodeDist/npm/"
+[String] $nodeExe = "node.exe"
+[String] $OSArch = ""
+$npmVersions = @()
+
+#Temporary variable
+[String] $Version = "latest/"
+
+function Get-EnvironmentVar
+{
     [CmdletBinding()]
     Param
     (
@@ -8,41 +20,150 @@
         [ValidateSet("User", "Machine", "Process")]
         [String]$Target
     )
-    
     $Target = @{$true="Process";$false=$Target}[$Target -eq $null -or $Target -eq ""]
     Write-Output ([System.Environment]::GetEnvironmentVariable($Variable, $Target))
  }
 
-
-
-function nvm 
+function Set-EnvironmentVar
 {
     [CmdletBinding()]
     Param
     (
+        [Parameter(Mandatory=$true)]
+        [String]$Variable,
+        [Parameter(Mandatory=$true)]
+        [String]$Value,
+        [ValidateSet("User", "Machine", "Process")]
+        [String]$Target
     )
 
     Begin
     {
-    
+        $Target = @{$true="Process";$false=$Target}[$Target -eq $null -or $Target -eq ""]
     }
-
     Process
     {
-    
+        [System.Environment]::SetEnvironmentVariable($Variable, $Value, $Target)
     }
-
     End
     {
+        Write-Output (Get-EnvironmentVar $Variable $Target)
+    }
+    
+ }
 
+function Install-Node 
+{
+    [CmdletBinding()]
+    Param
+    (
+        [ValidateScript({
+            
+        })]
+
+        [String]$Version = ""
+    )
+
+    $Version = @{$true="latest/"; $false="$Version"}[$Version -eq $null -or $Version -eq ""]
+
+    if((Test-Path "$($nvmHome)$($version)") -eq $false){
+        New-Item "$($nvmHome)$($version)" -ItemType Directory | Out-Null
+    }
+
+    if($env:HTTP_PROXY -eq $null){
+        curl "$($nodeDist)$($Version)$($OSArch)$($nodeExe)" -OutFile "$($nvmHome)$($Version)$($nodeExe)"
+    }
+    else{
+        curl "$($nodeDist)$($Version)$($OSArch)$($nodeExe)" -OutFile "$($nvmHome)$($Version)$($nodeExe)" -Proxy $env:HTTP_PROXY
     }
 }
 
-#Prereq checks
- if((Get-EnvironmentVar CurNodeVer Machine) -eq $null)
- {
-    #[System.Environment]::SetEnvironmentVariable("CurNodeVer", "Stable", "Machine")
-    Write-Output "var not ccreated"
- }
+function Update-Node
+{
+    Install-Node
+}
 
-Write-Output "PSNodeVersion module imported"
+function Change-Node
+{
+}
+
+function Parse-VersionNumber
+{
+    Param
+    (
+        [ValidatePattern("^[\d]+\.[\d]+.[\d]+$")]
+        [ValidateScript({ $npmVersions.Node -contains "v$($_)" })]
+        [String]$Version
+    )
+
+    Write-Output $Version
+}
+
+function Return-Versions
+{
+    Write-Output $npmVersions
+}
+
+#To implement
+function Start-Node{
+    node $args
+}
+
+#NPM wrapper function that start npm in node with args
+function npm 
+{
+    node "$($nvmHome)\node_modules\npm\bin\npm-cli.js" $args
+}
+
+
+#Set alias in module so curl always references to Invoke-WebRequest
+Set-Alias -Name curl -Value Invoke-WebRequest -Option AllScope
+set-alias -Name cat -Value Get-Content -option AllScope
+Set-Alias -Name node -Value "$($nvmHome)$($Version)$($nodeExe)"
+
+#Run Prereq Test
+if((Get-EnvironmentVar CurNodeVer Machine) -eq $null)
+{
+    #Set-EnvironmentVar "CurNodeVer" "latest" "Machine"
+    #$env:CurNodeVer = "latest"
+    Write-Output "var not created"
+}
+
+if((Test-Path $nvmHome) -eq $false)
+{
+    New-Item $nvmHome -ItemType Directory
+}
+
+if((Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq "64-Bit")
+{
+    $OSArch = "x64/"
+}
+    
+$matchedV = [regex]::Matches((cat .\npm-versions.txt), "(?<Node>v[\d]+\.[\d]+.[\d]+)\s(?<NPM>[\d]+\.[\d]+.[\d]+)")
+
+foreach($m in $matchedV){
+    $npmVersions += (@{ "Node"=$m.Groups["Node"].Value; "NPM"=$m.Groups["NPM"].Value })      
+}
+#-------------------------------------------------
+
+#Set-Alias -Name npm -Value "$($nvmHome)npm.cmd"
+
+#Export functions, vars ans alias
+Export-ModuleMember -Function Install-Node
+Export-ModuleMember -Function Update-Node
+Export-ModuleMember -Function Change-Node
+Export-ModuleMember -Function Start-Node
+Export-ModuleMember -Function Return-Versions
+Export-ModuleMember -Function Parse-VersionNumber
+
+#Make sure everything is installed on local drive
+#previously used $home to install to local dir
+#in some corporations the $home dir is a network share to sync documents,.. across computers
+#in this case using the $home dir introduced significant latency
+# -> decided to use a predifined dir on the c:/ drive -> find a better way and location to be sure that i don't install on share and do not need c root dir
+
+#find a way to introduce npmrc -> to point list -g to appdata roaming dir -> this is also used by default node installer
+Export-ModuleMember -Alias node
+Export-ModuleMember -Function npm
+
+Export-ModuleMember -Variable npmVersions
---
+++ SetVarTest.ps1
@@ -0,0 +1,47 @@
+﻿$e = "C:\Windows\system32\WindowsPowerShell\v1.0\;C:\Python34\;C:\Python34\Scripts;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\PROGRA~1\IBM\SQLLIB\BIN;C:\PROGRA~1\IBM\SQLLIB\FUNCTION;C:\Program Files (x86)\Belgium Identity Card;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\PrivateAssemblies\;C:\Program Files (x86)\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files\Microsoft Windows Performance Toolkit\;C:\Program Files (x86)\IBM\WebSphere MQ\bin64;C:\Program Files (x86)\IBM\WebSphere MQ\bin;C:\Program Files (x86)\IBM\WebSphere MQ\tools\c\samples\bin;C:\Program Files (x86)\Microsoft Team Foundation Server 2010 Power Tools\;C:\Program Files (x86)\Microsoft Team Foundation Server 2010 Power Tools\Best Practices Analyzer\;C:\Program Files (x86)\ZANTAZ\EAS Outlook Addin\bin;C:\Program Files\Microsoft\Web Platform Installer\;C:\Program Files (x86)\Microsoft ASP.NET\ASP.NET Web Pages\v1.0\;C:\Program Files (x86)\Windows Kits\8.0\Windows Performance Toolkit\;C:\Program Files\Microsoft SQL Server\110\Tools\Binn\;C:\Program Files (x86)\Microsoft Application Virtualization Client;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\Git\bin;C:\Program Files\nodejs"
+#$e = "C:\Windows\system32\WindowsPowerShell\v1.0\;C:\Python34\;C:\Python34\Scripts;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\PROGRA~1\IBM\SQLLIB\BIN;C:\PROGRA~1\IBM\SQLLIB\FUNCTION;C:\Program Files (x86)\Belgium Identity Card;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\Tools\Binn\;C:\Program Files\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\PrivateAssemblies\;C:\Program Files (x86)\Microsoft SQL Server\100\DTS\Binn\;C:\Program Files\Microsoft Windows Performance Toolkit\;C:\Program Files (x86)\IBM\WebSphere MQ\bin64;C:\Program Files (x86)\IBM\WebSphere MQ\bin;C:\Program Files (x86)\IBM\WebSphere MQ\tools\c\samples\bin;C:\Program Files (x86)\Microsoft Team Foundation Server 2010 Power Tools\;C:\Program Files (x86)\Microsoft Team Foundation Server 2010 Power Tools\Best Practices Analyzer\;C:\Program Files (x86)\ZANTAZ\EAS Outlook Addin\bin;C:\Program Files\Microsoft\Web Platform Installer\;C:\Program Files (x86)\Microsoft ASP.NET\ASP.NET Web Pages\v1.0\;C:\Program Files (x86)\Windows Kits\8.0\Windows Performance Toolkit\;C:\Program Files\Microsoft SQL Server\110\Tools\Binn\;C:\Program Files (x86)\Microsoft Application Virtualization Client;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\Git\bin;H:\NodeJS"
+
+ function Get-EnvironmentVar
+ {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String]$Variable,
+        [ValidateSet("User", "Machine", "Process")]
+        [String]$Target
+    )
+    $Target = @{$true="Process";$false=$Target}[$Target -eq $null -or $Target -eq ""]
+    Write-Output ([System.Environment]::GetEnvironmentVariable($Variable, $Target))
+ }
+
+ function Set-EnvironmentVar
+ {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String]$Variable,
+        [Parameter(Mandatory=$true)]
+        [String]$Value,
+        [ValidateSet("User", "Machine", "Process")]
+        [String]$Target
+    )
+
+    Begin
+    {
+        $Target = @{$true="Process";$false=$Target}[$Target -eq $null -or $Target -eq ""]
+    }
+    Process
+    {
+        [System.Environment]::SetEnvironmentVariable($Variable, $Value, $Target)
+    }
+    End
+    {
+        Write-Output (Get-EnvironmentVar $Variable $Target)
+    }
+    
+ }
+
+
+Set-EnvironmentVar "PATH" $e "Machine"
