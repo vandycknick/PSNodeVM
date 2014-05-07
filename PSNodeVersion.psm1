4c666df292d6a3ddb6d4cade5328c4b2d0d0acc4
vdyckn <vandyck.nick@outlook.com>
2014-05-07 10:38:16 +0200

    Changed internal curl function to fetch-http and improved flexibility of function

--- PSNodeVersion.psm1
+++ PSNodeVersion.psm1
@@ -28,7 +28,7 @@ function Install-Node
         New-Item "$($nvmHome)$($versionCopy)" -ItemType Directory | Out-Null
     }    
     
-    curl -Uri "$($nodeDist)$($versionCopy)$($OSArch)$($nodeExe)" -OutFile "$($nvmHome)$($versionCopy)$($nodeExe)"
+    Fetch-HTTP -Uri "$($nodeDist)$($versionCopy)$($OSArch)$($nodeExe)" -OutFile "$($nvmHome)$($versionCopy)$($nodeExe)"
 }
 
 function Update-Node
@@ -49,12 +49,10 @@ function Get-AllNodeVersions
 {
     [CmdletBinding()]
     Param
-    (
-
-    )
+    ()
     Begin
     {   
-        $nodeVPage = (curl -Uri "$($nodeDist)").Content
+        $nodeVPage = (Fetch-HTTP -Uri "$($nodeDist)").Content
     }
     Process
     {
@@ -80,7 +78,7 @@ function Install-Npm
 {
    if((Test-Path "$($nvmHome)node_modules\npm\bin\npm-cli.js") -eq $false)
    {
-        $npmInfo = (ConvertFrom-Json -InputObject (curl $npmDist))        
+        $npmInfo = (ConvertFrom-Json -InputObject (Fetch-HTTP $npmDist))        
 
         #Unzip-Archive -Source "$($nvmHome)npm-1.4.9.zip" -Destination $nvmHome
    }
@@ -119,21 +117,22 @@ function Set-EnvironmentVar
     Write-Output (Get-EnvironmentVar $Variable $Target)
  }
 
-function curl
+function Fetch-HTTP
 {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true, Position=0)]
         [String]$Uri,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false, Position=1)]
         [String]$OutFile
     )
+
     if($env:HTTP_PROXY -eq $null){
         Invoke-WebRequest -Uri $Uri -OutFile $OutFile
     }
     else{
-        Invoke-WebRequest -Uri $Uri -OutFile $OutFile  -Proxy $env:HTTP_PROXY
+        Invoke-WebRequest -Uri $Uri -OutFile $OutFile -Proxy $env:HTTP_PROXY
     }
 }
 
